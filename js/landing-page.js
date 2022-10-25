c6c1da75304df8c825622a18c664cdf3f3c6573b
Villy Rosa <villyrosa@hotmail.com>
2022-10-25 17:31:56 -0300

    Start Responsive

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,6 +1,8 @@
 var body = document.querySelector('body');
 var section = document.querySelector('section');
 var chevron = document.querySelector('.chevron');
+var linkChevron = document.querySelector('#linkChevron');
+var cont = 0;
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -15,5 +17,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 chevron.onclick = function() {
     section.classList.toggle('hidden');
     chevron.classList.toggle('updown');
-    window.location.href = '#sla';
 }
